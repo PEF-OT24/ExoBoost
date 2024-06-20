6b14f2e31656d207daeefaab15410eac21d442d0
carlosreyesb <carlos.reyesb@udem.edu>
2024-06-19 19:01:40 -0600

    AssistanceTab Change 19-06-24

--- app/test_design.py
+++ app/test_design.py
@@ -12,16 +12,17 @@ from kivy.core.window import Window
 from ColorManager import ColorManager
 import platform
 from kivy.clock import Clock
-
-Clock.max_iteration = 50  # Increase this value if necessary
+from kivymd.uix.label import MDLabel
+Clock.max_iteration = 1000  # Increase this value if necessary
 
 # Create multiple windows, main code will be located in main window
 # SecundaryWindow (as well as new created) might contain different or new functions to the app
 class MainWindow(Screen): pass 
 class SecundaryWindow(Screen): pass
 class WindowManager(ScreenManager): pass
+class CustomLabelAD(MDLabel): pass
 
-class TestDesignApp(MDApp):  
+class TestDesignApp(MDApp):
     def __init__(self, **kwargs):
         '''Initializes all methods, initial logical setup and define attributes'''
         super().__init__(**kwargs)
@@ -37,16 +38,18 @@ class TestDesignApp(MDApp):
         Available colors:
         Cyan, Dark Blue, Light Orange, Light Gray, Black, White.
         '''
- 
+
     def build(self):
         """Loads kivy design file"""
         if not(self.kv_loaded):
             self.root = Builder.load_file("test.kv")
             self.kv_loaded = True
         return self.root
-
-    def on_start(self): 
+    
+    def on_start(self):
         self.root.current = "Main Window"
+        # Bind the slider's value to a function
+        #self.root.ids.assistance_level_slider.bind(value=self.on_slider_value_change)
 
     def detect_os(self) -> str:
         '''Detects current OS of device and returns it as string'''
@@ -57,27 +60,28 @@ class TestDesignApp(MDApp):
             return "Windows"
         elif os_name == 'Darwin':
             return "MacOS"
-        elif os_name.startswith('Java'):
-            if 'android' in platform.java_ver()[3][0]:
-                return "Android"
         else:
             return "Unknown"
-        
-    def toggle_screen(self, state: bool) -> None:
-        '''Toggle between fullscreen and windowed mode depending on OS or selected mode'''
-        if self.os_name == "Linux" or self.os_name == "Windows" or not(state):
-            Window.fullscreen = False
-        if self.os_name == "Android" or state:
-            print("Fullscreen")
+
+    def pos_screen(self, screen):
+        '''Sets the screen position for each OS'''
+        os_name = self.detect_os()
+        if os_name == 'Linux':
+            # Maximize the window on Linux
+            Window.maximize()
+        elif os_name == 'Windows':
+            # Fullscreen mode on Windows
+            if screen == 0:
+                Window.fullscreen = False
+            else:
+                Window.fullscreen = True
+        elif os_name == 'MacOS':
+            # Fullscreen mode on MacOS
             Window.fullscreen = True
-    
-    def pos_screen(self, screen: int) -> None:
-        '''Position screen on desired window for debugging'''
-        if screen == 0: 
-            Window.left = 0
-        elif screen == 1: 
-            Window.left = 1920
-        Window.top = 30 
+
+    def on_slider_value(self, value):
+        '''Handle the slider value change'''
+        print(f"Slider value: {value}")
 
     def sit_down_stand_up(self):
         print("Sit down/stand up action triggered")
