0a9edd0088b356f18a07ae88b92c55ef6bb6b3ce
David Villanueva <david.villanueva.guzman@gmail.com>
2024-07-24 19:15:05 -0600

    code simplification

--- TivaC/can_test3/can_test3.ino
+++ TivaC/can_test3/can_test3.ino
@@ -126,6 +126,41 @@ void CAN0IntHandler(void) {
     }
 }
 
+void send_cmd(uint8_t ID, uint8_t *messageArray, bool show){
+  // Objetos para la comunicación CAN
+  tCANMsgObject Message_Tx;
+  tCANMsgObject Message_Rx;
+  uint8_t CAN_data_RX[8u];
+
+  // Define el mensaje para mandar por CAN
+  Message_Tx.ui32MsgID = 0x140 + ID;
+  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
+  Message_Tx.ui32MsgLen = 8u;
+  Message_Tx.pui8MsgData = messageArray;
+
+  // Define el mensaje para leer por CAN
+  Message_Rx.ui32MsgID = 0x240 + ID;
+  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
+  Message_Rx.ui32MsgLen = 8u;
+  Message_Rx.pui8MsgData = CAN_data_RX;
+
+  // Envío por CAN
+  CANMessageSet(CAN0_BASE, ID, &Message_Tx, MSG_OBJ_TYPE_TX); 
+
+  // Lee el mensaje de respuesta
+  CANMessageSet(CAN0_BASE, ID, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
+
+  // Imprime el mensaje si el usuario lo indica
+  if (show){
+    char buffer[50];
+    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
+    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
+            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
+            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
+    Serial.println(buffer);
+  }
+}
+
 void SendParameters(){
   // Se define la variable que almacena el valor
   uint8_t CANBUSSend_PIDvalues[8u];
@@ -164,42 +199,15 @@ void ReadParameters(int8_t ID, bool show){
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
-            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void set_speed(int8_t ID, int64_t speed_ref, bool show){
   
   // Objetos para la comunicación CAN
   tCANMsgObject Message_Tx;
-  tCANMsgObject Message_Rx;
   uint8_t CAN_data_TX[8u];
-  uint8_t CAN_data_RX[8u];
 
   // Escala el valor a enviar 0.01 dps/LSB
   int32_t sp = speed_ref * 100;
@@ -217,33 +225,7 @@ void set_speed(int8_t ID, int64_t speed_ref, bool show){
   CAN_data_TX[6] = byteArray[1];
   CAN_data_TX[7] = byteArray[0];
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
-            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
-    Serial.println(buffer);
-  }
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void read_acceleration(int8_t ID, bool show){
@@ -264,34 +246,10 @@ void read_acceleration(int8_t ID, bool show){
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
-            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
+
 void set_acceleration(int8_t ID, int64_t accel_ref, bool show){
   
   // Objetos para la comunicación CAN
@@ -316,33 +274,8 @@ void set_acceleration(int8_t ID, int64_t accel_ref, bool show){
   CAN_data_TX[6] = byteArray[1];
   CAN_data_TX[7] = byteArray[0];
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
-            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void set_absolute_position(int8_t ID, int32_t position_ref, int16_t max_speed, bool show){ // NO FUNCIONA
@@ -373,33 +306,8 @@ void set_absolute_position(int8_t ID, int32_t position_ref, int16_t max_speed, b
   CAN_data_TX[6] = byteArray_position[1];
   CAN_data_TX[7] = byteArray_position[0];
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[0], CAN_data_RX[1], CAN_data_RX[2], CAN_data_RX[3], 
-            CAN_data_RX[4], CAN_data_RX[5], CAN_data_RX[6], CAN_data_RX[7]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void set_incremental_position(int8_t ID, int32_t position_inc, int16_t max_speed, bool show){ 
@@ -430,36 +338,11 @@ void set_incremental_position(int8_t ID, int32_t position_inc, int16_t max_speed
   CAN_data_TX[6] = byteArray_position[1];
   CAN_data_TX[7] = byteArray_position[0];
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[7], CAN_data_RX[6], CAN_data_RX[5], CAN_data_RX[4], 
-            CAN_data_RX[3], CAN_data_RX[2], CAN_data_RX[1], CAN_data_RX[0]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
-void set_stposition(int8_t ID, int64_t position_inc, int64_t max_speed, int8_t direction, bool show){ 
+void set_stposition(int8_t ID, int16_t position_inc, int16_t max_speed, int8_t direction, bool show){ 
   
   // Objetos para la comunicación CAN
   tCANMsgObject Message_Tx;
@@ -480,40 +363,15 @@ void set_stposition(int8_t ID, int64_t position_inc, int64_t max_speed, int8_t d
   // Define el setpoint de la velocidad
   CAN_data_TX[0] = 0xA6;
   CAN_data_TX[1] = direction;
-  CAN_data_TX[2] = byteArray_speed[1];
-  CAN_data_TX[3] = byteArray_speed[0];
+  CAN_data_TX[2] = 0x02;
+  CAN_data_TX[3] = 0x02;
   CAN_data_TX[4] = byteArray_position[1];
   CAN_data_TX[5] = byteArray_position[0];
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[7], CAN_data_RX[6], CAN_data_RX[5], CAN_data_RX[4], 
-            CAN_data_RX[3], CAN_data_RX[2], CAN_data_RX[1], CAN_data_RX[0]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void set_torque(int8_t ID, int64_t current_torque, bool show){ 
@@ -541,35 +399,9 @@ void set_torque(int8_t ID, int64_t current_torque, bool show){
   CAN_data_TX[6] = 0;
   CAN_data_TX[7] = 0;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-
-  // Define el mensaje para leer por CAN
-  Message_Rx.ui32MsgID = 0x240 + ID;
-  Message_Rx.ui32MsgIDMask = 0xFFFFFFFF; // Lee todos los mensajes
-  Message_Rx.ui32MsgLen = 8u;
-  Message_Rx.pui8MsgData = CAN_data_RX;
-
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
-
-  // Lee el mensaje de respuesta
-  CANMessageSet(CAN0_BASE, 2, &Message_Rx, MSG_OBJ_TYPE_RXTX_REMOTE);
-
-  // Imprime el mensaje si el usuario lo indica
-  if (show){
-    char buffer[50];
-    while (CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST)) {Serial.println("waiting");}
-    sprintf(buffer, "Received: %02X %02X %02X %02X %02X %02X %02X %02X", 
-            CAN_data_RX[7], CAN_data_RX[6], CAN_data_RX[5], CAN_data_RX[4], 
-            CAN_data_RX[3], CAN_data_RX[2], CAN_data_RX[1], CAN_data_RX[0]);
-    Serial.println(buffer);
-  }
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
-
 
 void stop_motor(int8_t ID){
   
@@ -587,14 +419,8 @@ void stop_motor(int8_t ID){
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-  
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void shutdown_motor(int8_t ID){
@@ -613,14 +439,8 @@ void shutdown_motor(int8_t ID){
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-  
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 
@@ -640,14 +460,8 @@ void reset_motor(int8_t ID){
   CAN_data_TX[6] = 0x00;
   CAN_data_TX[7] = 0x00;
 
-  // Define el mensaje para mandar por CAN
-  Message_Tx.ui32MsgID = 0x140 + ID;
-  Message_Tx.ui32MsgIDMask = 0xFFFFFFFF;
-  Message_Tx.ui32MsgLen = 8u;
-  Message_Tx.pui8MsgData = CAN_data_TX;
-  
-  // Envío por CAN
-  CANMessageSet(CAN0_BASE, 1, &Message_Tx, MSG_OBJ_TYPE_TX); 
+  // Se envía el mensaje
+  send_cmd(ID, CAN_data_TX, show);
 }
 
 void loop() {
