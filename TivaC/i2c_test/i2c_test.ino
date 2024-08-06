5c4dd0103c5cea9fc9b88d8d8f09c571dd8a70e3
carlosreyesb <carlos.reyesb@udem.edu>
2024-08-05 20:30:17 -0600

    Update i2c_test.ino

--- TivaC/i2c_test/i2c_test.ino
+++ TivaC/i2c_test/i2c_test.ino
@@ -1,117 +1,44 @@
-#define PART_TM4C123GH6PM
 #include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
-#include "inc/hw_ints.h"
-#include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
-#include "driverlib/debug.h"
-#include "driverlib/gpio.h"
-#include "driverlib/fpu.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
+#include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/uart.h"
-#include "driverlib/pwm.h"
-#include "driverlib/adc.h"
-#include "driverlib/can.h"
-#include "driverlib/systick.h"
+#include "driverlib/gpio.h"
 #include "driverlib/i2c.h"
-#include "Wire.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/interrupt.h"
+#include <Wire.h>
 
-// LED Definitions
-#define RED_LED GPIO_PIN_1
-#define BLUE_LED GPIO_PIN_2
-#define GREEN_LED GPIO_PIN_3
+#define I2C_DEV_ADDR 0x55
 
-// Define CAN_INT_INTID_STATUS if not defined
-#ifndef CAN_INT_INTID_STATUS
-#define CAN_INT_INTID_STATUS 0x8000
-#endif
+uint32_t i = 0;
 
-uint32_t intensity = 0;
-uint8_t CANBUSSend_PIDvalues[8u];
-uint8_t CANBUSSend_Control[8u];
-uint8_t CANBUSSend_Stop[8u];
-uint8_t CANBUSReceive[8u];
-uint8_t inByte = 0;
-bool doControlFlag = 0;
+void onRequest() {
+  char message[32];
+  snprintf(message, sizeof(message), "%lu Packets.", i++);
+  Wire.write((uint8_t*)message, strlen(message));  // Send data as bytes
+  Serial.println("onRequest");
+  Serial.println(message);
+}
 
-tCANMsgObject sMsgObjectRx;
-tCANMsgObject sMsgObjectTx_PIDvalues;
-tCANMsgObject sMsgObjectTx_Control;
-tCANMsgObject sMsgObjectTx_Stop;
-
-// address of SLAVE
-#define SLAVE_ADDRESS          0x14
-// global variable to receive data
- uint32_t g_ui32DataRx;
+void onReceive(int len) {
+  Serial.print("onReceived: " + len);
+  while (Wire.available()) {
+    Serial.write(Wire.read());
+  }
+  Serial.println();
+}
 
 void setup() {
-    Serial.begin(9600);
-
-    // Enable peripherals
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) {}
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED);
-
-    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)) {}
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
-    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
-    ADCSequenceEnable(ADC0_BASE, 0);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)) {}
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_CAN0)) {}
-    CANInit(CAN0_BASE);
-    CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 1000000u);
-    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
-    CANEnable(CAN0_BASE);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)) {}
-    GPIOPinConfigure(GPIO_PB4_CAN0RX);
-    GPIOPinConfigure(GPIO_PB5_CAN0TX);
-    GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_I2C0)) {}
-    I2CSlaveIntEnableEx(I2C0_BASE, I2C_SLAVE_INT_DATA);
-    I2CSlaveEnable(I2C0_BASE);
-    I2CSlaveInit(I2C0_BASE, SLAVE_ADDRESS);
-
-    GPIOPinConfigure(GPIO_PB2_I2C0SCL);
-    GPIOPinConfigure(GPIO_PB3_I2C0SDA);
-    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
-    GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
-
-    IntMasterEnable();
+  Serial.begin(115200);
+  //Serial.setDebugOutput(true);
+  Wire.begin((uint8_t)I2C_DEV_ADDR);
+  Wire.onReceive(onReceive);
+  Wire.onRequest(onRequest);
 }
 
-void I2C0SlaveIntHandler(void)
-{
-    // Clear the I2C0 interrupt flag.
-    I2CSlaveIntClear(I2C0_BASE);
-    // Read the data from the slave.
-    g_ui32DataRx = I2CSlaveDataGet(I2C0_BASE);
-    I2CSlaveStatus(I2C0_BASE);
-}
-
-void loop(){
-    switch (g_ui32DataRx){
-        case 0x00:
-            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, LOW);
-            break;
-        case 0x01:
-            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, BLUE_LED);
-            break;
-    }
+void loop() {
+  // No need for anything in the loop
 }
