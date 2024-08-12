31e0045a6aa5cb42f5b86120c9dc26e7211c1b87
David Villanueva <david.villanueva.guzman@gmail.com>
2024-08-12 09:57:25 -0600

    Update BLE_ESP32.ino

--- ESP32/BLE_ESP32/BLE_ESP32.ino
+++ ESP32/BLE_ESP32/BLE_ESP32.ino
@@ -299,10 +299,45 @@ class BLECallback_MODE : public BLECharacteristicCallbacks {
   }
 };
 
+// Función para mandar un mensaje a través de I2C
+void sendI2CMessage(uint8_t slaveAddress, const char* message) {
+  int length = strlen(message);  // Calculate the length of the message
+  byte byteArray[length];        // Create a byte array of the same length
+
+  // Variables de estatus
+  int bytesWritten; // Longitud de los bytes escritos
+  int errorCode;    // Código de error después de escribir
+
+  // Se convierte el string a un arreglo de bytes
+  for (int i = 0; i < length; i++) {
+    byteArray[i] = (byte)message[i];
+  }
+
+  // Se imprime el mensaje a mandar
+  Serial.print("Sending message: ");
+  Serial.println(message);
+
+  // Se manda el mensaje al esclavo I2C
+  Wire.beginTransmission(slaveAddress);
+  bytesWritten = Wire.write(byteArray, length);  
+  errorCode = Wire.endTransmission();
+
+  // Se formatea la salida para debug
+  Serial.print("Mandando mensaje a dirección: ");
+  Serial.println(slaveAddress);
+  Serial.print("Código de error: ");
+  Serial.println(errorCode);
+  Serial.print("Bytes escritos: ");
+  Serial.println(bytesWritten);
+  Serial.println("----------------");
+}
+
 void setup() {
   // Inicializa el puerto serie para la depuración
   Serial.begin(115200);
   Serial.println("Iniciando el servidor BLE...");
+
+  Wire.begin();  // SDA = GPIO 21, SCL = GPIO 22 by default on ESP32
 
   // Inicializa el pin del LED
   pinMode(LED_PIN, OUTPUT);
@@ -412,4 +447,8 @@ void loop() {
   pCharacteristic_PV->setValue(values_buffer);
 
   // Serial.println(pCharacteristic_PV->getValue());
+
+  // Se manda información por I2C
+  const char* message2 = "ESP32 says Hi!";
+  sendI2CMessage(0x55, message2);
 }
