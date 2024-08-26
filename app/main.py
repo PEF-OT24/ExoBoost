88e4ecf030381b202d37226630dfd6472b5faa22
David Villanueva <david.villanueva.guzman@gmail.com>
2024-08-26 15:21:28 -0600

    larger delay

--- app/main.py
+++ app/main.py
@@ -143,7 +143,7 @@ class ExoBoostApp(MDApp):
         self.reading: bool = False                 # Indica si la lectura de datos se encuentra activa
 
         # Se inicializa el hilo secundario de la lectura de datos
-        self.read_thread = Thread(target=self.read_pv_cycle, args=(100,))
+        self.read_thread = Thread(target=self.read_pv_cycle, args=(1000,))
 
         # -------- Manejo de los UUID según la ESP32 ---------
         self.uuid_manager = UUIDManager() # Ver UUIDManager.py
